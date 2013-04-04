d92260c0a7a37d76c4a99d4f8b611d0a9ce12d6a
Ariol Grullon <ariolgrullon@gmail.com>
2013-04-04 13:19:52 -0400

    Loggin error!!
    Trate de hacer que tomara la foto pero me tira un error al momento de
    dale clic al boton de entrar en la Activity de loggin :(

--- src/practicas/rifa/New_Rifa.java
+++ src/practicas/rifa/New_Rifa.java
@@ -2,10 +2,14 @@ package practicas.rifa;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class New_Rifa extends Activity {
+	private static final int TOMAR_FOTO = 1;
+	ImageView imagen = (ImageView) findViewById(R.id.imageView1);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,21 @@ public class New_Rifa extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_rifa);
 	}
+	
+	public void tomarFoto(View view)
+	{
+		Intent TF = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(TF, TOMAR_FOTO);
+		
+	}
+	
+	@Override 
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+	                if  (requestCode == TOMAR_FOTO) {
+	                         Bitmap foto = (Bitmap) data.getExtras().get("data"); 
+	                         imagen.setImageBitmap(foto);
+	               }
+	 }
 	
 	public void cancelar1(View view) 
 	{
